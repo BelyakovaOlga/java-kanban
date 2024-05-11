ae2b0f500deaf1f419bb5bebf5a1e81d8a437f7a
olecheBel <ovbelyakova11@yandex.ru>
2024-05-11 14:38:48 +0300

    Модификаци по Sprint6 , исправления после автопроверок

--- src/service/Manager.java
+++ src/service/Manager.java
@@ -1,7 +1,8 @@
 package service;
 
 public class Manager {
-    public static TaskManager getDefault(){ return new InMemoryTaskManager(getDefaultHistory());
+    public static TaskManager getDefault(){
+        return new InMemoryTaskManager(getDefaultHistory());
     }
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
